dd7bc8230e8ab87610193934c4c8bae5e795a2fe
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-11-08 01:18:18 +0500

    Add files via upload

---
+++ ДЗ №29 до 11.11.23_CS_Классы/ДЗ №29 до 11.11.23_CS_Классы_Задание1.cs
@@ -0,0 +1,104 @@
+﻿//ДЗ №29 до 11.11.23_CS_Классы_Задание1
+/*
+ Задание 1. Разработать класс Rectangle, который будет хранить высоту,
+ширину и символ прямоугольника. Разработать у него конструктор по
+умолчанию, который будет создавать прямоугольник размер 1 на 1 с
+символом “*”, конструктор, который принимает высоту и ширину (символ
+становится “*”), и который принимает все три параметра.
+Создать метод Print, который выводит в консоль текстовым прямоугольник
+заданного размера заданным символом. Он принимает один параметр типа
+bool, если тот true, прямоугольник заполнен, если false - то нет.
+
+ */
+
+
+using System;
+
+namespace ДЗ__29_до_11._11._23_CS_Классы_Задание1
+{
+
+    class Rectangle
+{
+    private int height;
+    private int width;
+    private char symbol;
+
+    public Rectangle()
+    {
+        height = 1;
+        width = 1;
+        symbol = '*';
+    }
+
+    public Rectangle(int height, int width)
+    {
+        this.height = height;
+        this.width = width;
+        symbol = '*';
+    }
+
+    public Rectangle(int height, int width, char symbol)
+    {
+        this.height = height;
+        this.width = width;
+        this.symbol = symbol;
+    }
+
+    public void Draw()
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void Print(bool filled)
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (filled || i == 0 || i == height - 1 || j == 0 || j == width - 1)
+                {
+                    Console.Write(symbol);
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Rectangle defaultRectangle = new Rectangle();
+        defaultRectangle.Draw();
+        Console.WriteLine();
+
+        Rectangle customRectangle = new Rectangle(3, 5);
+        customRectangle.Draw();
+        Console.WriteLine();
+
+        Rectangle customSymbolRectangle = new Rectangle(4, 4, '#');
+        customSymbolRectangle.Draw();
+        Console.WriteLine();
+
+        Rectangle filledRectangle = new Rectangle(5, 10, '*');
+        filledRectangle.Print(true);
+        Console.WriteLine();
+
+        Rectangle hollowRectangle = new Rectangle(6, 8, '#');
+        hollowRectangle.Print(false);
+    }
+}
+
+}
---
+++ ДЗ №29 до 11.11.23_CS_Классы/ДЗ №29 до 11.11.23_CS_Классы_Задание2.cs
@@ -0,0 +1,102 @@
+﻿//ДЗ №29 до 11.11.23_CS_Классы_Задание2
+/*
+ Задание 2. Создайте класс Корзина, который хранит массив классов Продукт.
+Продукт имеет название и цену, может их выводить по методу Print, имеет
+соответствующие конструкторы. Корзина может вывести список продуктов, а
+также посчитать их суммарную стоимость, имеет конструктор по умолчанию,
+а также методы для добавления и удаления продуктов из корзины.
+ 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ДЗ__29_до_11._11._23_CS_Классы_Задание2
+{
+    class Product
+    {
+        public string Name { get; }
+        public double Price { get; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name}: ${Price}");
+        }
+    }
+
+    class Cart
+    {
+        private List<Product> products;
+
+        public Cart()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            products.Remove(product);
+        }
+
+        public void PrintProducts()
+        {
+            if (products.Count == 0)
+            {
+                Console.WriteLine("Корзина пуста.");
+                return;
+            }
+
+            Console.WriteLine("Продукты в корзине:");
+            foreach (var product in products)
+            {
+                product.Print();
+            }
+        }
+
+        public double CalculateTotalPrice()
+        {
+            return products.Sum(product => product.Price);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Product product1 = new Product("Яблоко", 1.0);
+            Product product2 = new Product("Банан", 0.5);
+            Product product3 = new Product("Молоко", 2.0);
+
+            Cart cart = new Cart();
+            cart.AddProduct(product1);
+            cart.AddProduct(product2);
+            cart.AddProduct(product3);
+
+            cart.PrintProducts();
+
+            double totalPrice = cart.CalculateTotalPrice();
+            Console.WriteLine($"Общая стоимость: ${totalPrice}");
+
+            cart.RemoveProduct(product2);
+
+            Console.WriteLine("После удаления продукта 'Банан':");
+            cart.PrintProducts();
+
+            totalPrice = cart.CalculateTotalPrice();
+            Console.WriteLine($"Общая стоимость: ${totalPrice}");
+        }
+    }
+
+}
