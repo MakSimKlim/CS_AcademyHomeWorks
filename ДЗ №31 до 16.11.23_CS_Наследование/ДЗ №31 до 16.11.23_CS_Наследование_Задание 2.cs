58a496e4df94c71d9571786cbdeb0e575d9b38bf
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-11-12 21:48:19 +0500

    Add files via upload

---
+++ ДЗ №31 до 16.11.23_CS_Наследование/ДЗ №31 до 16.11.23_CS_Наследование_Задание 2.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+class Device
+{
+    protected string Name;
+
+    public Device(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void Sound()
+    {
+        Console.WriteLine(GetSoundDescription());
+    }
+
+    public abstract string GetSoundDescription();
+
+    public virtual void Desc()
+    {
+        Console.WriteLine($"Описание {Name}: [Описание по-умолчанию]");
+    }
+
+    public override string ToString()
+    {
+        return $"Устройство: {Name}";
+    }
+}
+
+class Kettle : Device
+{
+    public Kettle(string name, string characteristic)
+        : base(name)
+    {
+        // Дополнительные характеристики для чайника
+        Characteristic = characteristic;
+    }
+
+    public string Characteristic { get; set; }
+
+    public override void Desc()
+    {
+        Console.WriteLine($"Описание {Name}: {Characteristic}");
+    }
+   public override string GetSoundDescription()
+    {
+        return $"{Name} производит звук кипящей воды.";
+    }
+}
+
+class Microwave : Device
+{
+    public Microwave(string name, string feature)
+        : base(name)
+    {
+        // Дополнительные характеристики для микроволновки
+        Feature = feature;
+    }
+
+    public string Feature { get; set; }
+
+    public override void Desc()
+    {
+        Console.WriteLine($"Описание {Name}: {Feature}");
+    }
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} производит звук колокольчика.");
+    }
+}
+
+class Car : Device
+{
+    public Car(string name, string model)
+        : base(name)
+    {
+        // Дополнительные характеристики для автомобиля
+        Model = model;
+    }
+
+    public string Model { get; set; }
+
+    public override void Desc()
+    {
+        Console.WriteLine($"Описание {Name}: {Model}");
+    }
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} производит звук двигателя.");
+    }
+}
+
+class Ship : Device
+{
+    public Ship(string name, string type)
+        : base(name)
+    {
+        // Дополнительные характеристики для парохода
+        Type = type;
+    }
+
+    public string Type { get; set; }
+
+    public override void Desc()
+    {
+        Console.WriteLine($"Описание {Name}: {Type}");
+    }
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} производит звук гудка.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Kettle kettle = new Kettle("Электрический чайник", "Кипятит воду");
+        Microwave microwave = new Microwave("Микроволновка", "Разогревает еду");
+        Car car = new Car("Машина", "Перемещается по дорогам");
+        Ship ship = new Ship("Пароход", "Перевозит грузы по воде");
+
+        Console.WriteLine(kettle);
+        kettle.Sound();
+        kettle.Desc();
+
+        Console.WriteLine();
+
+        Console.WriteLine(microwave);
+        microwave.Sound();
+        microwave.Desc();
+
+        Console.WriteLine();
+
+        Console.WriteLine(car);
+        car.Sound();
+        car.Desc();
+
+        Console.WriteLine();
+
+        Console.WriteLine(ship);
+        ship.Sound();
+        ship.Desc();
+    }
+}
