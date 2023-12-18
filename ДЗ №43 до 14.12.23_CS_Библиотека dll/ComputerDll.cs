d7fbabdb99d2e3bacfebcec718feaaad2761857a
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-12-18 20:48:38 +0500

    Add files via upload

---
+++ ДЗ №43 до 14.12.23_CS_Библиотека dll/ComputerDll.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerDll
+{
+
+    public class Computer
+    {
+        // Поля с автоматическими свойствами
+        public string Brand { get; set; }
+        public string SerialNumber { get; set; }
+        public string Generation { get; set; }
+
+        // Приватное поле для хранения состояния включения/выключения
+        private bool isTurnedOn;
+
+        // Конструкторы
+        public Computer()
+        {
+            // Конструктор по умолчанию
+            Brand = null;
+            SerialNumber = null;
+            Generation = null;
+            isTurnedOn = false;
+        }
+
+        public Computer(string brand, string serialNumber, string generation)
+        {
+            // Конструктор с параметрами
+            Brand = brand;
+            SerialNumber = serialNumber;
+            Generation = generation;
+            isTurnedOn = false;
+        }
+
+        // Методы
+        public void TurnOn()
+        {
+            if (!isTurnedOn)
+            {
+                Console.WriteLine($"{Brand} Computer with Serial Number {SerialNumber} is turning on.");
+                isTurnedOn = true;
+            }
+            else
+            {
+                Console.WriteLine($"{Brand} Computer is already turned on.");
+            }
+        }
+
+        public void TurnOff()
+        {
+            if (isTurnedOn)
+            {
+                Console.WriteLine($"{Brand} Computer with Serial Number {SerialNumber} is turning off.");
+                isTurnedOn = false;
+            }
+            else
+            {
+                Console.WriteLine($"{Brand} Computer is already turned off.");
+            }
+        }
+
+        public void Reboot()
+        {
+            if (isTurnedOn)
+            {
+                Console.WriteLine($"{Brand} Computer with Serial Number {SerialNumber} is rebooting.");
+                TurnOff();
+                TurnOn();
+            }
+            else
+            {
+                Console.WriteLine($"{Brand} Computer cannot be rebooted because it's turned off.");
+            }
+        }
+    }
+}
---
+++ ДЗ №43 до 14.12.23_CS_Библиотека dll/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using ComputerDll;
+
+namespace ДЗ__43_CS_Сборка_с_библиотекой_dll
+{
+    public class Serializator
+    {
+        public static void SerializeComputerList(List<Computer> computerList, string fileName)
+        {
+            try
+            {
+                using (FileStream fileStream = new FileStream(fileName, FileMode.Create))
+                {
+                    BinaryFormatter binaryFormatter = new BinaryFormatter();
+                    binaryFormatter.Serialize(fileStream, computerList);
+                }
+
+                Console.WriteLine($"Serialization successful. File saved as {fileName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Serialization error: {ex.Message}");
+            }
+        }
+    }
+
+    public class Deserializator
+    {
+        public static List<Computer> DeserializeComputerList(string fileName)
+        {
+            try
+            {
+                using (FileStream fileStream = new FileStream(fileName, FileMode.Open))
+                {
+                    BinaryFormatter binaryFormatter = new BinaryFormatter();
+                    return (List<Computer>)binaryFormatter.Deserialize(fileStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Deserialization error: {ex.Message}");
+                return null;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример использования сериализации
+            List<Computer> computersToSerialize = new List<Computer>
+            {
+            new Computer { Brand = "Brand1", SerialNumber = "SN1", Generation = "Gen1" },
+            new Computer { Brand = "Brand2", SerialNumber = "SN2", Generation = "Gen2" }
+        };
+
+            Serializator.SerializeComputerList(computersToSerialize, "ComputerList.dat");
+
+            // Пример использования десериализации
+            List<Computer> deserializedComputers = Deserializator.DeserializeComputerList("ComputerList.dat");
+
+            if (deserializedComputers != null)
+            {
+                foreach (var computer in deserializedComputers)
+                {
+                    Console.WriteLine($"Brand: {computer.Brand}, Serial Number: {computer.SerialNumber}, Generation: {computer.Generation}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
